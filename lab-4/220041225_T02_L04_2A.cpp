f6b8bd6e66c8356a22bf7c0fcc2d6363814ace2c
zannatulmaliha <147336779+zannatulmaliha@users.noreply.github.com>
2024-09-29 12:18:43 +0600

    Update 220041225_T02_L04_2A.cpp

--- lab-4/220041225_T02_L04_2A.cpp
+++ lab-4/220041225_T02_L04_2A.cpp
@@ -6,66 +6,104 @@ class BankAccount{
   string accountnumber;
   string acc_holder_name;
   string acc_type;
-  float current_balance;
-  float minimum;
-  float interest;
+  double current_balance;
+  const double minimum;
+  double interest;
   
   static int totalaccount_created;
   static int totalaccount_present;
+  static double totaltax_collected;
   
   public:
-  BankAccount():minimum(5){
-      totalaccount_created++;
-      totalaccount_present++;
-    }
-  BankAccount(string acc_num,string holder_name,string type,float balance):accountnumber(acc_num),acc_holder_name(holder_name),acc_type(type),minimum(0),current_balance(5){
-      
+
+  BankAccount(string acc_num,string holder_name,string type,double balance,double mini):accountnumber(acc_num),acc_holder_name(holder_name),acc_type(type),minimum(mini),current_balance(balance){
+      if(current_balance<minimum){
+          current_balance=minimum;
+          cout << "Initial balance too low. Reset to minimum balance: " << minimum << "\n";
+      }
        totalaccount_created++;
       totalaccount_present++;
   }
   
-  void showBalance(){
+   ~BankAccount(){
+      cout<<"Account of " <<acc_holder_name<<" with acc. no: "<<accountnumber<<" is destroyed with balance "<<current_balance<<"\n";
+       totalaccount_present--;
+  }
+  
+  
+  void showBalance()const{
       cout<<"Balance is: "<<current_balance<<"\n";
   }
-  void deposit(float x){
+  void deposit(double x){
+      if(x>0)
       current_balance+=x;
+      else
+      cout<<"give a valid value\n";
   }
-  void withdraw(float x){
+  void withdraw(double x){
       if((current_balance-x)<minimum){
           cout<<"Can't withdraw\n";
       }
       else
       current_balance-=x;
   }
-  void giveInterst(){
-      float inte=0.03*current_balance;
-      inte=0.9*inte;
-      current_balance+=inte;
+  void giveInterest(double inter=3.0){
+      double interest=(inter*current_balance)/100;
+      double source=0.1*interest;
+      interest-=source;
+      current_balance+=interest;
+      totaltax_collected+=source;
   }
-  ~BankAccount(){
-      cout<<"Account of " <<acc_holder_name<<" with acc. no: "<<accountnumber<<" is destroyed with balance "<<current_balance<<"\n";
-       totalaccount_present--;
-  }
-  int get(){
+     double getCurrentBalance() const {
+        return current_balance;
+    }
+ 
+  static int total_acc(){
       return totalaccount_present;
   }
-   int gettt(){
+  static int total_cre(){
       return totalaccount_created;
   }
+  static double total_tax(){
+      return totaltax_collected;
+  }
+  
 };
 
 int  BankAccount::totalaccount_created=0;
-int   BankAccount::totalaccount_present=0;
+int  BankAccount::totalaccount_present=0;
+double BankAccount::totaltax_collected=0.0;
 
-void display_stat(BankAccount p){
-     cout<<"total account present is "<<p.get()<<"\n";
-     cout<<"total account created is" <<p.gettt()<<"\n";
+void display_stat(){
+     cout<<"total account present is "<<BankAccount::total_acc()<<"\n";
+     cout<<"total account created is " <<BankAccount::total_cre()<<"\n";
+      cout<<"total source tax is " <<BankAccount::total_tax()<<"\n";
+}
+
+BankAccount Larger(const BankAccount A, const BankAccount B){
+    return (A.getCurrentBalance()>B.getCurrentBalance()?A:B);
 }
 
 int main(){
-    BankAccount B("544dsdsf","maliha","current",521.54);
-    BankAccount c,d,e;
-    display_stat(B);
+     BankAccount B("544dsdsf", "Maliha", "current", 521.54, 15.2);
+    BankAccount E("123", "Iha", "current", 1000.0, 7.2);
+    BankAccount D("4554", "Rashid", "savings", 454.54,45.3);
+    BankAccount C("578", "cat", "current", 475.3, 56.3);
+
+
+    B.giveInterest();
+    E.giveInterest();
+    D.giveInterest(3);
+    C.giveInterest(2);
+
+   
+    display_stat();
+
+   
+    BankAccount larger_acc = Larger(B, E);
+    cout << "Account with larger balance is: " << larger_acc.getCurrentBalance() << "\n";
+
+   
     
     return 0;
 }
